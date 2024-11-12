210804d822f4b658cb030cf40fb3621eda1bed7b
Willams S. de Sousa <44249322+osdeving@users.noreply.github.com>
2024-11-12 14:11:02 -0300

    ci: remove pasta backend

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import 'dotenv/config';
-import { startConsumer } from './kafkaConsumer.js';
-
-const environment = 'pre'; // Alterar para 'prod' conforme necessário
-const topic = 'events.biometry.qa1'; // Nome do tópico desejado
-
-startConsumer(environment, topic);
